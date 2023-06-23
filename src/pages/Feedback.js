3c268a1e6e7c72e677ce6dfb09ea372ced643129
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-06-23 18:24:36 -0300

    ✨ feat: implementa header na pagina de feedback (ainda não esta funcionando)

---
+++ src/pages/Feedback.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Header />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
