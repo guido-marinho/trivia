fff57e2caedbc579c13953f43029a5320194cecc
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-06-26 18:22:58 -0300

    ✨ feat: permite ir para a pagina de login a partir da pagina ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,25 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div>
+        <div data-testid="ranking-title">Ranking</div>
+        <button
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Voltar para a tela inicial
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
